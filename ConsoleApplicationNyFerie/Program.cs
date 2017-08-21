f483ef94e4f8c77c9d84100b5a71f28162ef9e16
Christian <Christian@WORKSTATION>
2017-08-21 18:13:56 +0200

    Add project files.

---
+++ ConsoleApplicationNyFerie/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplicationNyFerie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
