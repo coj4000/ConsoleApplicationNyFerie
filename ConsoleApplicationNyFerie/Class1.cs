ee533f1391f5fbbf82496b863fbc865ffea4cfb5
Christian <Christian@WORKSTATION>
2017-08-22 22:53:45 +0200

    Har lavet en simpel console app

---
+++ ConsoleApplicationNyFerie/Class1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplicationNyFerie
+{
+    class Class1
+    {
+        public int Alder { get; set; }
+        public string Navn { get; set; }
+        public DateTime Dato { get; set; }
+
+        public Class1()
+        {
+            Alder = 1;
+            Navn = "John Doe";
+            Dato = DateTime.Today;
+        }
+
+        public Class1(int alder, string navn)
+        {
+            Alder = alder;
+            Navn = navn;
+            Dato = DateTime.Today;
+        }
+        public string Person(Class1 personer)
+        {
+            string navnet = "Elev " + personer.Navn;
+            string alder = " er " + personer.Alder + " år gamle";
+            string personen = navnet + alder + " Den " + personer.Dato;
+            return personen;
+        }
+        
+    }
+}
--- ConsoleApplicationNyFerie/Program.cs
+++ ConsoleApplicationNyFerie/Program.cs
@@ -8,8 +8,33 @@ namespace ConsoleApplicationNyFerie
 {
     class Program
     {
+        static void ElevWork()
+        {
+            List<Class1> listeClass = new List<Class1>();
+            Class1 nyElev = new Class1(22, "Frank");
+            Class1 nyElev1 = new Class1(23, "John");
+            listeClass.Add(nyElev);
+            listeClass.Add(nyElev1);
+            
+            foreach (var person in listeClass)
+            {
+                string elev = person.Person(person);
+                Console.WriteLine($"Denne {elev}");
+            }
+
+        }
+
         static void Main(string[] args)
         {
+            try
+            {
+                ElevWork();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                throw;
+            }
         }
     }
 }
